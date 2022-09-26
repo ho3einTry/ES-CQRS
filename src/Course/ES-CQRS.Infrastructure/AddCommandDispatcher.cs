e4b1442e0d8ac8b223086c11682b08ac60d2678f
hossein <ho3einTry@gmail.com>
2022-09-26 15:01:47 +0330

    Add Pipeline Dispatcher

---
+++ src/Course/ES-CQRS.Infrastructure/AddCommandDispatcher.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ES_CQRS.Infrastructure;
+
+public static class AddCommandDispatcher
+{
+    public static void Add(this IServiceCollection services)
+    {
+        
+    }
+}
---
+++ src/Course/ES-CQRS.Infrastructure/AddQueryDispatcher.cs
@@ -0,0 +1,6 @@
+namespace ES_CQRS.Infrastructure;
+
+public class AddQueryDispatcher
+{
+    
+}
