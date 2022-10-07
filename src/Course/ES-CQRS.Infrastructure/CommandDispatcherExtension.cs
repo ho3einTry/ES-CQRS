80b9b275f84f1d72cbdac2649603d00744dcb67e
striver <ho3einTry@gmail.com>
2022-10-07 16:54:19 +0330

    Fix CommandHandler Dispatcher

--- src/Course/ES-CQRS.Infrastructure/CommandDispatcherExtension.cs
+++ src/Course/ES-CQRS.Infrastructure/CommandDispatcherExtension.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using ES_CQRS.Handlers;
 using ES_CQRS.Seedwork;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,7 +10,7 @@ public static class CommandDispatcherExtension
 {
     public static void AddCommandDispatcher(this IServiceCollection services)
     {
-        services.AddMediatR(Assembly.GetExecutingAssembly());
+        services.AddMediatR(Assembly.GetAssembly(typeof(AddCourseCommandHandler))!);
         services.AddSingleton<ICommandDispatcher,MediatRCommandDispatcher>();
       //  services.AddScoped<typeof(ICommandHandler<Add>)>()
     }
--- src/ES-CQRS.Seedwork/ICommandHandler.cs
+++ src/ES-CQRS.Seedwork/ICommandHandler.cs
@@ -5,6 +5,5 @@ namespace ES_CQRS.Seedwork;
 public interface ICommandHandler<in TCommand> : INotificationHandler<TCommand>
     where TCommand : class, ICommand
 {
-  
     Task HandelAsync(TCommand command, CancellationToken cancellationToken = default);
 }
